66e2c81f41fde5da202a01c8086cf3ec2507e16b
yunaiju <hyejoo04@naver.com>
2025-07-28 15:06:54 +0900

    :tada: init

---
+++ src/main/java/com/sample/gihubActions/Controller.java
@@ -0,0 +1,13 @@
+package com.sample.gihubActions;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+@org.springframework.stereotype.Controller
+public class Controller {
+    @ResponseBody
+    @GetMapping("")
+    public String main() {
+        return "hello";
+    }
+}
---
+++ src/main/java/com/sample/gihubActions/GihubActionsApplication.java
@@ -0,0 +1,13 @@
+package com.sample.gihubActions;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GihubActionsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GihubActionsApplication.class, args);
+	}
+
+}
